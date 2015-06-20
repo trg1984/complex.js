87723e0710c4fe6a7dcc97c78a0ea25934d7191b
Rolf Lindén <rolind@utu.fi>
2015-06-20 12:00:27 +0300

    Added a heap of trigonometric functions; Added shorthands for function names; Lot of checks and bug fixes.

--- complex.js
+++ complex.js
@@ -2,7 +2,7 @@
  * 
  * A complex number class for Javascript.
  * 
- * Copyright 2013 Rolf Lindén (rolind@utu.fi). All rights reserved.
+ * Copyright 2013--2015 Rolf Lindén (rolind@utu.fi). All rights reserved.
  * 
  * Free to use, modify and distribute for no cost, as long as this
  * disclaimer remains in its place and unmodified (no changes,
@@ -17,8 +17,13 @@
  */
 
 Math.Complex = function(x, y) {
-    this.x = x;
-    this.y = y ? y : 0;
+	if (x instanceof Math.Complex) {
+		this.x = x.x;
+		this.y = x.y;
+	} else {
+		this.x = x;
+		this.y = typeof(y) === 'number' ? y : 0;
+	}
 }
 
 Math.Complex.prototype.meps = 1e-7;
@@ -62,44 +67,334 @@ Math.Complex.prototype.modulus = function() { return ( Math.sqrt(Math.pow(this.x
 
 Math.Complex.prototype.isReal = function() { return ( this.precision(Math.Complex.prototype.mepsExp).y === 0 ); }
 
+Math.Complex.prototype.isImaginary = function() { return ( this.precision(Math.Complex.prototype.mepsExp).y !== 0 ); }
+
+Math.Complex.prototype.isPurelyImaginary = function() { return ( this.precision(Math.Complex.prototype.mepsExp).x === 0 ); }
+
 Math.Complex.prototype.conjugate = function() { return ( new Math.Complex(this.x, -this.y) ); }
 
 Math.Complex.prototype.neg = function() { return ( new Math.Complex(-this.x, -this.y) ); }
 
 Math.Complex.prototype.inv = function() { return ( (new Math.Complex(1, 0)).div(this) ); }
 
-Math.Complex.prototype.add = function(a) { return ( new Math.Complex(this.x + a.x, this.y + a.y) ); }
+Math.Complex.prototype.add = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return ( new Math.Complex(this.x + temp.x, this.y + temp.y) );
+}
 
-Math.Complex.prototype.sub = function(a) { return ( new Math.Complex(this.x - a.x, this.y - a.y) ); }
+Math.Complex.prototype.sub = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return ( new Math.Complex(this.x - temp.x, this.y - temp.y) );
+}
 
-Math.Complex.prototype.mul = function(a) { return ( new Math.Complex(this.x * a.x - this.y * a.y, this.x * a.y + this.y * a.x) ); }
+Math.Complex.prototype.mul = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return ( new Math.Complex(this.x * temp.x - this.y * temp.y, this.x * temp.y + this.y * temp.x) );
+}
 
 Math.Complex.prototype.div = function(a) {
     
-    var conj = a.conjugate();
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	
+    var conj = temp.conjugate();
     var numerator = this.mul(conj);
-    var denominator = a.mul(conj);
+    var denominator = temp.mul(conj);
     
     return ( new Math.Complex(numerator.x / denominator.x, numerator.y / denominator.x) );
 }
 
+Math.Complex.prototype.ln = function(k) {
+	var mul = typeof(k) === 'number' ? k | 0 : (k instanceof Math.Complex) && (k.isReal()) ? k.x : 0;
+	
+	var r = this.modulus();
+	var angle = this.arg();
+
+	return new Math.Complex(Math.log(r), angle + 2 * mul * Math.PI);
+}
+
+Math.Complex.prototype.sin = function() {
+	if (this.isReal()) {
+		return (
+			Math.Complex.div(
+				Math.Complex.sub(
+					Math.Complex.pow(Math.E, Math.Complex.I.mul(this.x)),
+					Math.Complex.pow(Math.E, Math.Complex.I.mul(-this.x))
+				),
+				Math.Complex.I.mul(2)
+			)
+		);
+	} else {
+		return (
+			Math.Complex.add(
+				Math.Complex.mul(
+					Math.Complex.sin(this.x),
+					Math.Complex.cosh(this.y)
+				),
+				Math.Complex.mul(
+					Math.Complex.cos(this.x),
+					Math.Complex.sinh(this.y)
+				)
+			)
+		);
+	}
+}
+
+Math.Complex.prototype.cos = function() {
+	if (this.isReal()) {
+		return (
+			Math.Complex.div(
+				Math.Complex.add(
+					Math.Complex.pow(Math.E, Math.Complex.I.mul(this.x)),
+					Math.Complex.pow(Math.E, Math.Complex.I.mul(-this.x))
+				),
+				Math.Complex.I.mul(2)
+			)
+		);
+	} else {
+		return (
+			Math.Complex.sub(
+				Math.Complex.mul(
+					Math.Complex.cos(this.x),
+					Math.Complex.cosh(this.y)
+				),
+				Math.Complex.mul(
+					Math.Complex.sin(this.x),
+					Math.Complex.sinh(this.y)
+				)
+			)
+		);
+	}
+}
+
+Math.Complex.prototype.tan = function() {
+	// (e^(2iz)-1)/(i(e^(2iz)+1)),
+	return (
+		Math.Complex.div(
+			Math.Complex.sub(
+				Math.Complex.pow(
+					Math.E,
+					this.mul(Math.Complex.I.mul(2))
+				),
+				1
+			),
+			Math.Complex.add(
+				Math.Complex.pow(
+					Math.E,
+					this.mul(Math.Complex.I.mul(2))
+				),
+				1
+			)
+		)
+	);
+}
+
+Math.Complex.prototype.tanh = function() {
+	return (
+		Math.Complex.div(
+			Math.Complex.sinh(this),
+			Math.Complex.cosh(this)
+		)
+	);
+}
+
+Math.Complex.prototype.coth = function() {
+	return (
+		Math.Complex.div(
+			Math.Complex.cosh(this),
+			Math.Complex.sinh(this)
+		)
+	);
+}
+
+Math.Complex.prototype.sech = function() {
+	return (
+		Math.Complex.div(
+			1,
+			Math.Complex.cosh(this)
+		)
+	);
+}
+
+Math.Complex.prototype.csch = function() {
+	return (
+		Math.Complex.div(
+			1,
+			Math.Complex.sinh(this)
+		)
+	);
+}
+
+Math.Complex.prototype.sinh = function() {
+	return (
+		Math.Complex.div(
+			Math.Complex.sub(
+				Math.Complex.pow(Math.E, this),
+				Math.Complex.pow(Math.E, this.neg())
+			),
+			2
+		)
+	);
+}
+
+Math.Complex.prototype.cosh = function() {
+	return (
+		Math.Complex.div(
+			Math.Complex.add(
+				Math.Complex.pow(Math.E, this),
+				Math.Complex.pow(Math.E, this.neg())
+			),
+			2
+		)
+	);
+}
+
+Math.Complex.prototype.arcsin = function() {
+	// arcsin(z) = -i ln(iz +sqrt(1-z^2))
+	return (
+		Math.Complex.mul(
+			Math.Complex.I.neg(),
+			Math.Complex.ln(
+				Math.Complex.add(
+					Math.Complex.I.mul(this),
+					Math.pow(this.mul(this).neg().add(1), 1 / 2)
+				)
+			)
+		)
+	);
+}
+
+Math.Complex.prototype.arccos = function() {
+	// arccos(z) = -i ln(z +sqrt(z^2 - 1))
+	return (
+		Math.Complex.mul(
+			Math.Complex.I.neg(),
+			Math.Complex.ln(
+				Math.Complex.add(
+					this,
+					Math.pow(this.mul(this).sub(1), 1 / 2)
+				)
+			)
+		)
+	);
+}
+
+Math.Complex.prototype.arctan = function() {
+	// arctan(z) = i/2*(ln(1 - iz) - ln(1 + iz))
+	return (
+		Math.Complex.sub(
+			Math.Complex.mul(
+				Math.Complex.I.div(2),
+				Math.Complex.sub(
+					Math.Complex.ln(Math.Complex.sub(1, Math.Complex.I.mul(this))),
+					Math.Complex.ln(Math.Complex.add(1, Math.Complex.I.mul(this)))
+				)
+			)
+		)
+	);
+}
+
+Math.Complex.prototype.arccot = function() {
+	// arccot(z) = i/2*(ln(1 - i/z) - ln(1 + i/z))
+	return (
+		Math.Complex.sub(
+			Math.Complex.mul(
+				Math.Complex.I.div(2),
+				Math.Complex.sub(
+					Math.Complex.ln(Math.Complex.sub(1, Math.Complex.I.div(this))),
+					Math.Complex.ln(Math.Complex.add(1, Math.Complex.I.div(this)))
+				)
+			)
+		)
+	);
+}
+
+Math.Complex.prototype.arccsc = function() {
+	// arccsc(z) = -i ln(sqrt( 1-1/(z^2) ) + i/z)
+	return (
+		Math.Complex.mul(
+			Math.Complex.I.neg(),
+			Math.Complex.ln(
+				Math.Complex.add(
+					Math.pow(
+						Math.Complex.sub(1, (new Math.Complex(1)).div(this.mul(this))),
+						1 / 2
+					),
+					Math.Complex.I.div(this)
+				)
+			);
+		)
+	);
+}
+
+Math.Complex.prototype.arcsec = function() {
+	// arcsec(z) = -i ln(sqrt( 1/(z^2) - 1 ) + i/z)
+	return (
+		Math.Complex.mul(
+			Math.Complex.I.neg(),
+			Math.Complex.ln(
+				Math.Complex.add(
+					Math.pow(
+						Math.Complex.sub((new Math.Complex(1)).div(this.mul(this)), 1),
+						1 / 2
+					),
+					Math.Complex.I.div(this)
+				)
+			);
+		)
+	);
+}
+
+/* incomplete
+Math.Complex.prototype.gamma = function() {
+	var p = [
+		676.5203681218851,
+		-1259.1392167224028,
+		771.32342877765313,
+		-176.61502916214059,
+		12.507343278686905,
+		-0.13857109526572012,
+		9.9843695780195716e-6,
+		1.5056327351493116e-7
+	];
+
+	var z0 = new Math.Complex(this);
+
+	if (z0.Re() < 0.5) return Math.PI / (Math.Complex.sin(z0.mul(Math.PI)) * Math.Complex.gamma(1 - z0));
+	else {
+		
+		z0.x -= 1;
+		var x = 0.99999999999980993;
+		
+		for (i in p) {
+			x = Math.Complex.add(x, Math.Complex.div(p[i], z0.add(i + 1)));
+		}
+		t = z0.add(p.length - 0.5);
+		return t.pow(z0 + 0.5).mul(Math.sqrt(2 * Math.PI)).mul(Math.Complex.pow(Math.E, -t)).mul(x);
+	}
+}
+*/
 Math.Complex.prototype.pow = function(power) {
     
     var r = this.modulus();
     var angle = this.arg();
-    
-    return(
-        (new Math.Complex(
-            Math.pow(r, power) * Math.cos( angle * power ),
-            Math.pow(r, power) * Math.sin( angle * power )
-        )).precision(Math.Complex.prototype.mepsExp)
-    );
+	var pwr = power instanceof Math.Complex ? power : new Math.Complex(power);
+	var scalar = Math.pow(r, pwr.x) * Math.pow(Math.E, -pwr.y * angle);
+	var inner = angle * pwr.x + 0.5 * pwr.y * Math.log(Math.pow(r, 2));
+	
+	return(
+		(new Math.Complex(
+			scalar * Math.cos(inner),
+			scalar * Math.sin(inner)
+		)).precision(Math.Complex.prototype.mepsExp)
+	);
 }
 
 Math.Complex.prototype.toString = function() {
-    if (this.x == 0) return ( this.y + 'ⅈ' );
-    else if (this.y == 0) return ( this.x );
-    else return ( this.x + (this.y >= 0 ? '+' : '') + this.y + 'ⅈ' );
+	if (Number.isNaN(this.x) || Number.isNaN(this.y)) return 'NaN';
+	if (this.modulus() == 0) return '0';
+	var realPortion = this.isPurelyImaginary() ? '' : this.x;
+	var imaginaryPortion = this.isReal() ? '' : this.y == 1 ? 'i' : this.y == -1 ? '-i' : this.y + 'i';
+	return ( realPortion + ((this.y > 0) && (realPortion !== '') && (imaginaryPortion !== '') ? '+' : '') + imaginaryPortion );
 }
 
 Math.Complex.prototype.latex = function() {
@@ -128,4 +423,147 @@ Math.Complex.prototype.nthRoots = function(n, onlyRealRoots) {
     if (onlyRealRoots) roots.sort( function(a, b) { return(a.x - b.x); } ); // Sort real roots to ascending order. 
     
     return ( roots );
+}
+
+// Added for convinience.
+Math.Complex.I = new Math.Complex(0, 1);
+
+Math.Complex.add = function(a, b) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.add(b)
+}
+
+Math.Complex.sub = function(a, b) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.sub(b)
+}
+
+Math.Complex.mul = function(a, b) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.mul(b)
+}
+
+Math.Complex.div = function(a, b) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.div(b)
+}
+
+Math.Complex.neg = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.neg()
+}
+
+Math.Complex.inv = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.inv(b)
+}
+
+Math.Complex.sin = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.sin()
+}
+
+Math.Complex.cos = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.cos()
+}
+
+Math.Complex.tan = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.tan()
+}
+
+Math.Complex.arcsin = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.arcsin()
+}
+
+Math.Complex.arccos = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.arccos()
+}
+
+Math.Complex.arctan = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.arctan()
+}
+
+Math.Complex.arccot = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.arccot()
+}
+
+Math.Complex.arccsc = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.arccsc()
+}
+
+Math.Complex.arcsec = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.arcsec()
+}
+
+Math.Complex.sinh = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.sinh()
+}
+
+Math.Complex.cosh = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.cosh()
+}
+
+Math.Complex.tanh = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.tanh()
+}
+
+Math.Complex.coth = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.coth()
+}
+
+Math.Complex.sech = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.sech()
+}
+
+Math.Complex.csch = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.csch()
+}
+
+Math.Complex.ln = function(a, k) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.ln(k)
+}
+/* incomplete
+Math.Complex.gamma = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.gamma()
+}
+*/
+Math.Complex.pow = function(a, exponent) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.pow(exponent)
+}
+
+Math.Complex.conjugate = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.conjugate()
+}
+
+Math.Complex.arg = function(a) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.arg()
+}
+
+Math.Complex.add = function(a, b) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.add(b)
+}
+
+Math.Complex.nthRoots = function(a, n, onlyRealRoots) {
+	var temp = a instanceof Math.Complex ? a : new Math.Complex(a);
+	return temp.nthRoots(n, onlyRealRoots)
 }
